0eb463883266bf02eed9a07f0c03fcbeea850580
ny <ny567@naver.com>
2023-06-15 17:48:42 +0900

    230615 수업끝

--- src/component/TodoMain.js
+++ src/component/TodoMain.js
@@ -3,12 +3,19 @@ import TodoItem from './TodoItem';
 
 import './scss/TodoMain.scss';
 
-const TodoMain = () => {
+const TodoMain = ({ todoList, remove, check }) => {
+  // console.log(props.todoList);
+
   return (
     <ul className='todo-list'>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {
+          todoList.map(todo => <TodoItem 
+                                  key={todo.id} 
+                                  item={todo} 
+                                  remove={remove}
+                                  check={check}
+                                />)
+        }
     </ul>
   )
 }
