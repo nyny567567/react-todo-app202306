a950ea5cd8bddccb4dabf5b5f42d72ccc95146b8
ny <ny567@naver.com>
2023-06-22 17:53:19 +0900

    230622 수업끝

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import TodoTemplate from './component/TodoTemplate';
+import Footer from './component/layout/Footer';
+import Header from './component/layout/Header';
+import TodoTemplate from './component/todo/TodoTemplate';
+import Join from './component/user/Join';
+import Login from './component/user/Login';
 
 function App() {
   return (
-    <TodoTemplate />
+    <>
+      <Header />
+      
+      <Routes>
+        <Route path='/' element={ <TodoTemplate /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/join' element={ <Join /> } />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
